c98223b1b323a0432205c7ddf28f00feb7f31aac
egorfuntusov <funtushan@gmail.com>
2023-08-09 21:28:37 +0300

    maven

---
+++ unit_2_base_operations/src/main/java/ua/com/alevel/BaseOperations.java
@@ -0,0 +1,10 @@
+package ua.com.alevel;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class BaseOperations {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.upperCase("Hello BaseOperations!!!"));
+    }
+}
